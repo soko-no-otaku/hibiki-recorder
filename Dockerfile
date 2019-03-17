f067e02d3f1a3d539bfc58a94d7422f074942b3e
soko-no-otaku <soko.no.otaku@gmail.com>
2019-03-17 13:38:45 +0900

    Include rec_hibiki.rb in the image

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,10 @@ ENV TZ="Asia/Tokyo"
 
 RUN apt-get update && apt-get install -y ffmpeg
 
-WORKDIR /output
-COPY ./Gemfile Gemfile
+RUN mkdir /output
+
+COPY Gemfile /
 RUN bundle install
 
-ENTRYPOINT ["ruby", "./rec_hibiki.rb"]
+COPY rec_hibiki.rb /
+ENTRYPOINT ["ruby", "rec_hibiki.rb"]
--- rec_hibiki.rb
+++ rec_hibiki.rb
@@ -30,14 +30,14 @@ class HiBiKiRecorder
     updated_date = Time.parse(program_info['episode_updated_at']).strftime('%Y%m%d')
     program_name = program_info['name'].strip
     episode_name = program_info['latest_episode_name']
-    "#{updated_date}_#{program_name}_#{episode_name}"
+    "output/#{updated_date}_#{program_name}_#{episode_name}.ts"
   end
 
   def download_latest_episode(access_id)
     program_info = get_program_info(access_id)
     video_id = program_info['episode']['video']['id']
     playlist_url = get_playlist_url(video_id)
-    `ffmpeg -y -i #{Shellwords.escape(playlist_url)} -c copy #{Shellwords.escape("#{output_filename(program_info)}.ts")}`
+    `ffmpeg -y -i #{Shellwords.escape(playlist_url)} -c copy #{Shellwords.escape(output_filename(program_info))}`
   end
 end
 
